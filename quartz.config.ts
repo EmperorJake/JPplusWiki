bbef28cb64c5650c7f64604de19dcf8d0c60c3a1
EmperorJake <68182631+EmperorJake@users.noreply.github.com>
2026-03-09 03:32:26 +1100

    Quartz sync: Mar 9, 2026, 3:32 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "emperorjake.github.io/JPplusWiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
